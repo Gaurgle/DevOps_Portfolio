f9c4608fc02a1863be7f8334c2ef2aee647e3f4b
Gaurgle <justwannagroove@gmail.com>
2025-09-21 13:52:49 +0200

    adding react

---
+++ portfolio_FE/src/components/Hello.jsx
@@ -0,0 +1,3 @@
+export default function Hello({name}) {
+    return <div className="text-blue-500">Hello, {name}!</div>;
+}
---
+++ portfolio_FE/tailwind.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    content: [
+        './src/**/*.{astro,html,js,jsx,ts,tsx}' // Adjust paths as needed.
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
