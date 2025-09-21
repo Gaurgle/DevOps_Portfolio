ef35c5983557698a9d6eda1a19b1334dc1d10ec6
Gaurgle <justwannagroove@gmail.com>
2025-09-21 16:44:49 +0200

    banner, Astro fixing

---
+++ portfolio_FE/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,jsx,ts,tsx,astro}",
+        "./components/**/*.{html,js,jsx,ts,tsx,astro}", // Adjust to include relevant directories
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
--- portfolio_FE/tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-    export default {
-    content: [
-        './src/**/*.{astro,html,js,jsx,ts,tsx}' // Adjust paths as needed.
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
