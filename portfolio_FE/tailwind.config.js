5cac4a14659aaf20e19280adc60e6925163696f5
Gaurgle <justwannagroove@gmail.com>
2025-09-21 22:42:58 +0200

    added react component from hibbuba.io

--- portfolio_FE/tailwind.config.js
+++ portfolio_FE/tailwind.config.js
@@ -6,7 +6,18 @@ export default {
         "./layouts/**/*.{html,js,jsx,ts,tsx,astro}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                "border-beam": {
+                    "100%": {
+                        "offset-distance": "100%",
+                    },
+                },
+            },
+            animation: {
+                "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+            },
+        },
     },
     plugins: [],
 };
