32e5ff7f826fffa93c5f3347d52b95a37f6d62b7
Gaurgle <justwannagroove@gmail.com>
2025-09-21 22:33:07 +0200

    fixed tailwind

--- portfolio_FE/tailwind.config.js
+++ portfolio_FE/tailwind.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+// tailwind.config.js
+export default {
     content: [
         "./src/**/*.{html,js,jsx,ts,tsx,astro}",
-        "./components/**/*.{html,js,jsx,ts,tsx,astro}", // Adjust to include relevant directories
+        "./components/**/*.{html,js,jsx,ts,tsx,astro}",
+        "./layouts/**/*.{html,js,jsx,ts,tsx,astro}",
     ],
     theme: {
         extend: {},
