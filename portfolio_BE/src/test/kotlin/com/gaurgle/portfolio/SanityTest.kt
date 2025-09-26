183b69b2896dae5bebabb19d888851a5e98d6681
Gaurgle <justwannagroove@gmail.com>
2025-09-26 14:41:46 +0200

    test: sanity test for triggering CI

---
+++ portfolio_BE/src/test/kotlin/com/gaurgle/portfolio/SanityTest.kt
@@ -0,0 +1,12 @@
+package com.gaurgle.portfolio
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class SanityTest {
+
+    @Test
+    fun `one plus one is two`() {
+        assertEquals(2, 1 + 1)
+    }
+}
